6b1a5c7d9e1a1c72bd30973f8385d14bc536d87d
LOVEYOUFOREVER\liuyang <liuyanglg@outlook.com>
2017-07-26 21:33:57 +0800

    更改项目结构，并添加一个简单的分页查询

--- src/main/java/com/lyf/ssm/dao/StudentDao.java
+++ src/main/java/com/lyf/ssm/dao/StudentDao.java
@@ -1,8 +1,12 @@
 package com.lyf.ssm.dao;
 
+import com.lyf.ssm.entity.Page;
 import com.lyf.ssm.entity.Student;
+
+import java.util.List;
 
 
 public interface StudentDao {
     Student get(String sid);
+    List<Student> queryByPage(Page<Student> page);
 }
