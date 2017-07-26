b7d90605ff83add0c0a2f7c22c09df36ec36a550
LOVEYOUFOREVER\liuyang <liuyanglg@outlook.com>
2017-07-26 22:25:55 +0800

    由于配置文件位置改变，运行出错，错误位置：  <!--<property name="mapperLocations" value="classpath:com/**/mapper/*.xml"></property>-->

--- src/test/com/lyf/ssm/dao/StudentDaoTest.java
+++ src/test/com/lyf/ssm/dao/StudentDaoTest.java
@@ -1,9 +1,12 @@
 package com.lyf.ssm.dao;
 
 import com.lyf.base.BaseTest;
+import com.lyf.ssm.entity.Page;
 import com.lyf.ssm.entity.Student;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 public class StudentDaoTest extends BaseTest{
     @Autowired
@@ -12,5 +15,18 @@ public class StudentDaoTest extends BaseTest{
     public void get() throws Exception {
         Student student=studentDao.get("13011315");
         System.out.println(student);
+    }
+
+    @Test
+    public void queryByPage() throws Exception{
+        Student student=new Student();
+        student.setName("陈");
+        Page<Student> page = new Page<Student>(student);
+        page.setStartIndex(0);
+        page.setPageSize(10);
+        List<Student> list = studentDao.queryByPage(page);
+        for(Student stu:list){
+            System.out.println(stu);
+        }
     }
 }
